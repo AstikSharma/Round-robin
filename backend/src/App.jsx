e4c7b5a1cf11cbf33bbc45b42646860fa2487706
Astik Sharma <astiksharma26@gmail.com>
2025-03-13 14:43:13 +0530

    Latest commit

---
+++ backend/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import CouponClaim from "./components/CouponClaim";
+
+export default function App() {
+  return(
+    <>
+      <Header/>
+      <CouponClaim/>
+    </>
+  );
+}
